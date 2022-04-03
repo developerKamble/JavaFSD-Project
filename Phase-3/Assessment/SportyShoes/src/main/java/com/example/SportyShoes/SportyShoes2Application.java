4c47493dcd024162f5964fa497968df1e3e2383d
developerKamble <komaltanuja2714@gmail.com>
2022-04-03 17:12:07 +0530

    SportyShoes project

---
+++ Phase-3/Assessment/SportyShoes/src/main/java/com/example/SportyShoes/SportyShoes2Application.java
@@ -0,0 +1,13 @@
+package com.example.SportyShoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyShoes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoes2Application.class, args);
+	}
+
+}
